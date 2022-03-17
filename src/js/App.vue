169ecedf779f3ca25dcfe68f37996f8da75b8846
cherryReptile <lisovskiy.nikitk@gmail.com>
2022-03-18 00:09:47 +0300

    register view

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,20 +1,6 @@
 <template>
   <div>
-
-    <vs-input v-model="text" placeholder="Введите текст">
-      <template #icon>
-        <i class='bx bx-injection'></i>
-      </template>
-    </vs-input>
-
-    <div v-if="text === 'пидор'">
-      Сам ты пидор
-    </div>
-    <div v-else>
-      Текст, который вы ввели в инпуте: <b>{{ text }}</b>
-    </div>
-
-    <register></register>
+      <register></register>
   </div>
 </template>
 
@@ -26,7 +12,6 @@ export default {
   props: [],
   data() {
     return {
-      text: ''
     }
   },
   methods: {
--- src/js/components/register.vue
+++ src/js/components/register.vue
@@ -1,19 +1,31 @@
 <template>
-  <div class="center ">
+  <div class="center">
     <div class="grid">
       <vs-row justify="center">
         <vs-col>
-          <vs-input v-model="value1" placeholder="User name">
+          <vs-input v-model="userName" placeholder="User name">
             <template #icon>
               <i class='bx bx-user'></i>
             </template>
           </vs-input>
-
-          <vs-input type="password" icon-after v-model="value2" placeholder="Password">
+          <vs-input type="email" v-model="email" placeholder="Email">
+            <template #icon>
+              <i class='bx bx-injection'></i>
+            </template>
+          </vs-input>
+          <vs-input type="password" icon-before v-model="password" placeholder="Password">
             <template #icon>
               <i class='bx bx-lock-open-alt'></i>
             </template>
           </vs-input>
+          <vs-button
+              id="form-button"
+              shadow
+              :active="active == 0"
+              @click="active = 0"
+          >
+            Send
+          </vs-button>
         </vs-col>
       </vs-row>
     </div>
@@ -23,5 +35,14 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      userName: '',
+      email: '',
+      password: '',
+      active: 0
+    }
+  }
+}
 </script>
