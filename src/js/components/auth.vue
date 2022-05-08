43037a478db1bf68f9f088a24bec12bb464941d7
cherryReptile <lisovskiy.nikitk@gmail.com>
2022-05-08 14:19:43 +0300

    auth component

--- src/js/components/auth.vue
+++ src/js/components/auth.vue
@@ -1,9 +1,29 @@
 <template>
-  <auth-wrapper>
-    asdasdasd
+  <auth-wrapper @submit="submit">
+    <div ref="form">
+      <vs-input block type="email" v-model="form.email" placeholder="Email">
+        <template #icon>
+          <i class='bx bx-injection'></i>
+        </template>
+        <template v-if="errors.email" #message-danger>
+          {{ errors.email }}
+        </template>
+      </vs-input>
+      <vs-input block type="password" icon-before v-model="form.password" placeholder="Пароль">
+        <template #icon>
+          <i class='bx bx-lock-open-alt'></i>
+        </template>
+        <template v-if="errors.password" #message-danger>
+          {{ errors.password }}
+        </template>
+      </vs-input>
+    </div>
     <template #description>
-      test
-      <vs-button @click="$store.commit('SET_COMPONENT', 'register')">
+      <p class="text">Пожалуйста,авторизуйтесь,если уже зарегистрированы</p>
+      <p>Ещё не зарегистрированы?</p>
+      <vs-button
+          @click="$store.commit('SET_COMPONENT', 'register')"
+          gradient>
         Регистрация
       </vs-button>
     </template>
@@ -16,8 +36,63 @@
 
 <script>
 import AuthWrapper from "./auth-wrapper";
+
 export default {
-  components: {AuthWrapper}
+  components: {AuthWrapper},
+  methods: {
+    resetErrors: function () {
+      this.errors.email = ''
+      this.errors.password = ''
+    },
+    resetInputs: function () {
+      this.form.email = ''
+      this.form.password = ''
+    },
+
+    submit: function () {
+      const loading = this.$vs.loading({
+        target: this.$refs.form,
+        color: 'primary',
+      })
+
+      this.$http.post('http://speeddial.loc/api/auth/login', this.form)
+          .then(response => {
+            this.resetErrors()
+            this.resetInputs()
+            loading.close()
+            console.log(response.data)
+          })
+          .catch(error => {
+            this.resetErrors()
+            this.$vs.notification({
+              icon: '<i class=\'bx bx-error\'></i>',
+              color: 'danger',
+              position: 'top-right',
+              title: 'Ошибка авторизации',
+              text: error.response.data.message
+            })
+
+            for (let key in error.response.data.errors) {
+              this.errors[key] = error.response.data.errors[key][0]
+            }
+            loading.close()
+          })
+      loading.close()
+    }
+  },
+  data() {
+    return {
+      form: {
+        email: '',
+        password: ''
+      },
+      errors: {
+        email: '',
+        password: ''
+      },
+      active: 0
+    }
+  }
 }
 </script>
 
